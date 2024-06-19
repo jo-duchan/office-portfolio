6d8852aaf4387414d711c8a215ba3c0dfccea130
jo-duchan <jo_duchan@icloud.com>
2024-06-19 23:13:08 +0900

    color tokens, text styles 작업

---
+++ styles/typography.ts
@@ -0,0 +1,115 @@
+import { css } from "styled-components";
+
+const heading3 = {
+  bold: css`
+    font-size: 22px;
+    line-height: normal;
+    font-weight: 700;
+  `,
+};
+
+const label1 = {
+  bold: css`
+    font-size: 14px;
+    font-weight: 700;
+    line-height: normal;
+  `,
+  medium: css`
+    font-size: 14px;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const label2 = {
+  bold: css`
+    font-size: 12px;
+    font-weight: 700;
+    line-height: normal;
+  `,
+  medium: css`
+    font-size: 12px;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.5%;
+  `,
+  regular: css`
+    font-size: 12px;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const body1 = {
+  medium: css`
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+  regular: css`
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const body2 = {
+  medium: css`
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+  regular: css`
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const body3 = {
+  medium: css`
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+  regular: css`
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const body4 = {
+  medium: css`
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+  regular: css`
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 130%;
+    letter-spacing: -0.5%;
+  `,
+};
+
+const textStyles = {
+  heading3,
+  label1,
+  label2,
+  body1,
+  body2,
+  body3,
+  body4,
+};
+
+export default textStyles;
