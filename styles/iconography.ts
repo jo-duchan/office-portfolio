41c88e12e370a8b2f22ba763531797e378e86eb0
jo-duchan <jo_duchan@icloud.com>
2024-06-21 11:28:28 +0900

    iconography 작업

---
+++ styles/iconography.ts
@@ -0,0 +1,31 @@
+import Heading from "@/public/assets/icon/icon-heading.svg";
+import Text from "@/public/assets/icon/icon-text.svg";
+import Image from "@/public/assets/icon/icon-image.svg";
+import Gap from "@/public/assets/icon/icon-gap.svg";
+import AddImage from "@/public/assets/icon/icon-add-image.svg";
+import RemoveImage from "@/public/assets/icon/icon-remove-image.svg";
+import Add from "@/public/assets/icon/icon-add.svg";
+import Close from "@/public/assets/icon/icon-close.svg";
+import More from "@/public/assets/icon/icon-more.svg";
+import DropDown from "@/public/assets/icon/icon-drop-down.svg";
+import AlignCenter from "@/public/assets/icon/icon-align-center.svg";
+import AlignLeft from "@/public/assets/icon/icon-align-left.svg";
+import AlignRight from "@/public/assets/icon/icon-align-right.svg";
+
+const Icons = {
+  heading: Heading,
+  text: Text,
+  image: Image,
+  gap: Gap,
+  addImage: AddImage,
+  removeImage: RemoveImage,
+  add: Add,
+  close: Close,
+  more: More,
+  dropdown: DropDown,
+  alignCenter: AlignCenter,
+  alignLeft: AlignLeft,
+  alignRight: AlignRight,
+};
+
+export default Icons;
