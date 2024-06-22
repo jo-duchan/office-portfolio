5c508ec32b81880b961b5344f136399eb3dc0287
jo-duchan <jo_duchan@icloud.com>
2024-06-22 11:45:57 +0900

    auth page 작업

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Head from "next/head";
 import { signIn } from "@/libs/firebase/auth";
 import { useForm, type FieldValues } from "react-hook-form";
 import { useRouter } from "next/router";
 import PATH from "@/constants/path";
+import useModal from "@/hooks/useModal";
+import TextField from "@/components/common/TextField";
 
 export default function AdminAuthPage() {
-  const { register, handleSubmit, reset } = useForm<FieldValues>();
+  const { modal, showModal } = useModal();
+  const { register, handleSubmit } = useForm<FieldValues>();
 
   const router = useRouter();
   const handleSignIn = async (data: FieldValues) => {
-    const result = await signIn(data.id, data.pass);
+    const result = await signIn(data.id, data.password);
 
     if (result?.ok) {
       router.push(PATH.ADMIN);
     }
   };
+
+  useEffect(() => {
+    const modalContent = (
+      <>
+        <TextField
+          register={register}
+          name="id"
+          label="ID"
+          placeholder="아이디를 입력해 주세요."
+        />
+        <TextField
+          register={register}
+          name="password"
+          label="Password"
+          placeholder="암호를 입력해 주세요."
+          type="password"
+        />
+      </>
+    );
+    showModal({
+      title: "Sign In",
+      children: modalContent,
+      actionLabel: "Sign In",
+      action: handleSubmit(handleSignIn),
+    });
+  }, []);
+
   return (
-    <div>
-      Admin Auth
-      <form onSubmit={handleSubmit(handleSignIn)}>
-        <input type="text" {...register("id")} />
-        <input type="text" {...register("pass")} />
-        <button>로그인</button>
-      </form>
-    </div>
+    <>
+      <Head>
+        <title>Admin Auth</title>
+      </Head>
+      {modal}
+    </>
   );
 }
